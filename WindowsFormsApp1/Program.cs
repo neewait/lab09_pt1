ebcbc582b16e0d996c60febede72aa916661532a
neewait <korotaev.pika@gmail.com>
2023-12-28 01:15:52 +0300

    added

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace l9_CS
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string API_KEY = "5e9529dfc67ed27293e38ca71793f3c4";
+        private ComboBox comboBoxCities;
+        private ListBox listBoxCities;
+        public Form1()
+        {
+            Components();
+        }
+
+        private void Components()
+        {
+            Width = 400;
+            Height = 300;
+            Text = "Weather App";
+
+            comboBoxCities = new ComboBox();
+            comboBoxCities.Dock = DockStyle.Top;
+
+            listBoxCities = new ListBox();
+            listBoxCities.Width = 500;
+            listBoxCities.Height = 500;
+            listBoxCities.Left = 150;
+            listBoxCities.Top = 200;
+
+            var btnFetchCities = new Button();
+            btnFetchCities.Text = "Show Cities";
+            btnFetchCities.Width = 500;
+            btnFetchCities.Height = 100;
+            btnFetchCities.Left = 150;
+            btnFetchCities.Top = 100;
+            btnFetchCities.Click += BtnFetchCitiesClick;
+
+            var btnFetchWeather = new Button();
+            btnFetchWeather.Text = "Show Weather";
+            btnFetchWeather.Width = 500;
+            btnFetchWeather.Height = 100;
+            btnFetchWeather.Left = 850;
+            btnFetchWeather.Top = 100;
+            //TODO: implement click +=;
+            btnFetchWeather.Click += BtnFetchWeatherClick;
+
+            Controls.Add(btnFetchCities);
+            Controls.Add(btnFetchWeather);
+            Controls.Add(listBoxCities);
+        }
+
+        private async void BtnFetchCitiesClick(object sender, EventArgs e)
+        {
+            listBoxCities.Text = "";
+            var cities = await LoadCitiesFromFile("D:\\input\\city.txt");
+            listBoxCities.Items.AddRange(cities.ToArray());
+        }
+
+        private async Task<List<string>> LoadCitiesFromFile(string path)
+        {
+
+            List<string> datas = new List<string>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = await reader.ReadLineAsync();
+                    datas.Add(line);
+                }
+            }
+            List<string> cities = new List<string>();
+            for (int i = 0; i < datas.Count(); ++i)
+            {
+                int count = 0;
+                for (int j = 0; j < datas[i].Length; ++j)
+                {
+                    if (datas[i][j].Equals('\t'))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        ++count;
+                    }
+                }
+                cities.Add(datas[i].Substring(0, count));
+            }
+            return cities;
+        }
+
+        private async void BtnFetchWeatherClick(object sender, EventArgs e)
+        {
+            string city = listBoxCities.SelectedItem.ToString();
+
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("Please select a city.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                Weather weather = await FetchWeather(city);
+                MessageBox.Show($"{weather.Country} - {weather.Name}: {weather.Temp - 273}°C, {weather.Description}",
+                    "Weather Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task<Weather> FetchWeather(string city)
+        {
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonData = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(jsonData);
+
+                    string country = json["sys"]["country"].ToString();
+                    string name = json["name"].ToString();
+                    double temp = Convert.ToDouble(json["main"]["temp"]);
+                    string description = json["weather"][0]["description"].ToString();
+
+                    return new Weather
+                    {
+                        Country = country,
+                        Name = name,
+                        Temp = temp,
+                        Description = description
+                    };
+                }
+                else
+                {
+                    throw new Exception($"HTTP error: {response.StatusCode}");
+                }
+            }
+        }
+    }
+
+    struct Weather
+    {
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public double Temp { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ lab09/ts1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LAB9_CS
+{
+    internal class Program
+    {
+        private static readonly object fileLock = new object();
+        static async Task Main(string[] args)
+        {
+            List<string> tickers = ReadDataFromFile("C:\\vsc\\ticker.txt");
+            List<Task> tasks = new List<Task>();
+
+            foreach (string ticker in tickers)
+            {
+                tasks.Add(ProcessStock(ticker));
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        static async Task ProcessStock(string ticker)
+        {
+            DateTime endDate = DateTime.Now;
+            DateTime startDate = endDate.AddYears(-1);
+            long startUnixTime = (long)(startDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            long endUnixTime = (long)(endDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            string API_URL = $"https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={startUnixTime}&period2={endUnixTime}&interval=1d&events=history&includeAdjustedClose=true";
+            try
+            {
+                var data = await DownloadStockPrice(API_URL);
+
+                decimal average = 0;
+                if (data.Count > 0)
+                {
+                    average = data.Sum() / data.Count;
+                }
+
+                WriteToFile("C:\\vsc\\output.txt", $"{ticker}:{average}");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Error downloading stock data for {ticker}: {ex.Message}");
+            }
+
+            await Task.Delay(1000);
+        }
+
+        static async Task<List<int>> DownloadStockPrice(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Дабы не было ошибки 401
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer 5b46292ae01f2ba3b57add22869ad86ed5306bfe");
+                string data = await client.GetStringAsync(url);
+                string[] lines = data.Split('\n');
+                List<int> prices = new List<int>();
+
+                foreach (string line in lines.Skip(1))
+                {
+                    string[] columns = line.Split(',');
+
+                    try
+                    {
+                        int high = Int32.Parse(columns[2].Split('.')[0]);
+                        int low = Int32.Parse(columns[3].Split('.')[0]);
+                        int average = high + low / 2;
+                        prices.Add(average);
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine($"Error parsing value: {ex.Message}");
+                    }
+                }
+
+                return prices;
+            }
+        }
+
+        private static List<string> ReadDataFromFile(string path)
+        {
+            List<string> data = new List<string>();
+            if (File.Exists(path))
+            {
+                data.AddRange(File.ReadAllLines(path));
+            }
+            else
+            {
+                Console.WriteLine("File does not exist");
+            }
+
+            return data;
+        }
+
+        private static void WriteToFile(string path, string data)
+        {
+            lock (fileLock)
+            {
+                File.AppendAllText(path, data + '\n');
+            }
+        }
+    }
+}
